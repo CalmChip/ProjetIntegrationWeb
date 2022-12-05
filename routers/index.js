cbc30e48b7171912687dbfdfa7c826d8500c0058
Jean-Bernard Le Blanc <108352489+CalmChip@users.noreply.github.com>
2022-12-05 13:51:26 -0500

    Added post product route

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { Mongoose } = require("mongoose");
 const router = express.Router();
-const Products = require("../models/products");
+// const Products = require("../models/products");
+const { isAuthorized, isSeller } = require("../configs/auth");
 
 //Routers for main web page rendering
 router.get("/", (request, response) => {
@@ -36,9 +38,22 @@ router.get("/index.html", (request, response) => {
   });
 });
 
-/* // Router to post product into the DB
-router.post("/products", (request, response) => {
-
-}); */
+// Router to post product into the DB
+router.post("/products", isAuthorized, isSeller, (request, response) => {
+  const { productName, type, price, desc } = request.body;
+  let newProduct = {
+    _id: new Mongoose.ObjectId(),
+    productName: productName,
+    type: type,
+    price: price,
+    desc: desc,
+  };
+  newProduct.save().then((product) => {
+    request.flash("succes_msg", "Product added successfully");
+    response.redirect("accueil").catch((err) => {
+      console.log(err);
+    });
+  });
+});
 
 module.exports = router;
