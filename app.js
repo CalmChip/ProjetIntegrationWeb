6dcdf998c0326be4b3d924637febb11101f6a0b8
Jean-Bernard Le Blanc <108352489+CalmChip@users.noreply.github.com>
2022-12-11 17:47:42 -0500

    changed cart to carts

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((requete, reponse, next) => {
 // mes routes
 app.use("/", require("./routers/index"));
 app.use("/users", require("./routers/users"));
-app.use("/cart", require("./routers/carts"));
+app.use("/carts", require("./routers/carts"));
 
 //Statique route
 app.use("/css", express.static("./styles"));
